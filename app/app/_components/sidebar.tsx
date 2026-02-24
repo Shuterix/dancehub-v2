1a1d235c96ef4f7c6abf3454f1a5496ec75f47c5
Matúš Lupták <lupmatusak@gmail.com>
2026-02-24 13:49:31 +0100

    .

--- app/app/_components/sidebar.tsx
+++ app/app/_components/sidebar.tsx
@@ -344,7 +344,7 @@ export function DashboardSidebarLayout({
 	useEffect(() => {
 		if (prevPathnameRef.current !== pathname) {
 			prevPathnameRef.current = pathname
-			setMobileOpen(false)
+			queueMicrotask(() => setMobileOpen(false))
 		}
 	}, [pathname])
 
